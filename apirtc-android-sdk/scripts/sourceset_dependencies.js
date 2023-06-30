f467344ba28fdd6924602041c6b1eb1a5c90ad7c
Frédéric Luart <frederic.luart@apizee.com>
2023-06-30 15:54:02 +0200

    Upgrade apiRTC Android SDK doc - 1.1.4

---
+++ apirtc-android-sdk/scripts/sourceset_dependencies.js
@@ -0,0 +1,1 @@
+sourceset_dependencies='{":apiRTC:dokkaHtml/androidTestRelease":[],":apiRTC:dokkaHtml/debug":[],":apiRTC:dokkaHtml/main":[],":apiRTC:dokkaHtml/release":[],":apiRTC:dokkaHtml/testFixtures":[],":apiRTC:dokkaHtml/testFixturesDebug":[],":apiRTC:dokkaHtml/testFixturesRelease":[]}'
